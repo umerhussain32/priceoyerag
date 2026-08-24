c16fd793f92a1133efbe39ebc3fa891a09c46029
umerhussain32 <greenking3216@gmail.com>
2026-08-24 19:33:11 +0500

    Change model from meta-llama to openai/gpt-oss

--- retrieval_pipeline.py
+++ retrieval_pipeline.py
@@ -23,7 +23,7 @@ vectorstore = PineconeVectorStore(
 )
 
 llm = ChatOpenAI(
-    model="meta-llama/llama-prompt-guard-2-86m",
+    model="openai/gpt-oss-120b",
     openai_api_key=os.getenv("GROQ_API_KEY"),
     openai_api_base="https://api.groq.com/openai/v1",
     temperature=0.1
