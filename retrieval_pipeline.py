fae511d4d2c17d28130713a86c052d7b699d659c
umerhussain32 <greenking3216@gmail.com>
2026-08-24 18:38:30 +0500

    Update retrieval_pipeline.py

--- retrieval_pipeline.py
+++ retrieval_pipeline.py
@@ -23,7 +23,7 @@ vectorstore = PineconeVectorStore(
 )
 
 llm = ChatOpenAI(
-    model="meta-llama/llama-prompt-guard-2-22m",
+    model="openai/gpt-oss-safeguard-20b",
     openai_api_key=os.getenv("GROQ_API_KEY"),
     openai_api_base="https://api.groq.com/openai/v1",
     temperature=0.1
